3ad26e3cbe677ba9ee2f248a8e2b87c7616cab57
Jirka Mayer <jirkamayer97@gmail.com>
2024-06-28 18:26:19 -0400

    Basic video uploading to server

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,10 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app import __version__
+import aiofiles
+import os
 
 
 description = """
@@ -16,8 +19,41 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/", response_class=HTMLResponse)
 def index():
     path = os.path.join(os.path.dirname(__file__), "index.html")
     with open(path) as f:
         return f.read()
+
+
+# File uploading code
+########################
+# https://stackoverflow.com/questions/73442335/how-to-upload-a-large-file-%E2%89%A53gb-to-fastapi-backend
+
+CHUNK_SIZE = 1024 * 1024  # adjust the chunk size as desired
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    try:
+        filepath = os.path.join("storage", os.path.basename(file.filename))
+        async with aiofiles.open(filepath, "wb") as f:
+            while chunk := await file.read(CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception:
+        raise HTTPException(
+            status_code=500, 
+            detail="There was an error uploading the file"
+        )
+    finally:
+        await file.close()
+
+    return {"message": f"Successfuly uploaded {file.filename}"}
--- frontend/src/ui/IndexPage.tsx
+++ frontend/src/ui/IndexPage.tsx
@@ -1,5 +1,48 @@
 import { Box } from "@mui/joy";
+import { ChangeEvent, useState } from "react";
 
 export function IndexPage() {
-  return <Box>Hello world! This is the index page!</Box>;
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  function onFileChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files === null) {
+      return;
+    }
+
+    const file: File = e.target.files[0];
+    setSelectedFile(file);
+  }
+
+  function onFileUpload() {
+    if (selectedFile === null) {
+      return;
+    }
+
+    const formData = new FormData();
+
+    formData.append(
+      "file",
+      selectedFile.slice(0, selectedFile.size),
+      selectedFile.name
+    );
+
+    fetch("http://localhost:1817/upload", {
+      method: "POST",
+      body: formData,
+    });
+  }
+
+  return (
+    <Box>
+      Hello world! This is the index page!
+
+      <div>
+        <input type="file" onChange={onFileChange} />
+        <button onClick={onFileUpload}>
+          Upload!
+        </button>
+      </div>
+    </Box>
+  );
 }
