a2450f0139929fca37a504e124f1560c80841f79
Jirka Mayer <jirkamayer97@gmail.com>
2024-07-03 18:38:41 -0400

    Uploading recorded video

--- frontend/src/ui/RecordVideoPage.tsx
+++ frontend/src/ui/RecordVideoPage.tsx
@@ -1,22 +1,21 @@
 import { Box, Button } from "@mui/joy";
 import { WebcamRecorder } from "./WebcamRecorder";
 import { useState } from "react";
+import { BackendApi } from "../api/BackendApi";
 
 export function RecordVideoPage() {
   const [videoBlob, setVideoBlob] = useState<Blob | null>(null);
 
-  function uploadVideo() {
+  async function uploadVideo() {
     if (videoBlob === null) {
       return;
     }
 
-    // TODO
-    // Create a dummy download link for now
-    const chunkUrl = URL.createObjectURL(videoBlob);
-    const a = document.createElement("a");
-    a.innerText = "Click this link to download the recorded video";
-    a.href = chunkUrl;
-    document.body.appendChild(a);
+    const api = BackendApi.current();
+    await api.videos.upload(
+      videoBlob,
+      "recorded-video",
+    );
   }
 
   return (
