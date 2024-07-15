28bac788b06eeb40a80c84f1f0b4a1ef2cdd8dc8
Jirka Mayer <jirkamayer97@gmail.com>
2024-07-15 17:50:43 -0400

    Bare bones of d3js animation

--- frontend/src/ui/VideoRenderer.tsx
+++ frontend/src/ui/VideoRenderer.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/joy";
 import { useRef } from "react";
 import { VideoFile } from "../api/model/Video";
 import { useVideoFrameTracking } from "./useVideoFrameTracking";
+import * as d3 from "d3";
 
 export interface VideoRendererProps {
   readonly videoFile: VideoFile;
@@ -12,10 +13,19 @@ export interface VideoRendererProps {
 export function VideoRenderer(props: VideoRendererProps) {
   const videoElementRef = useRef<HTMLVideoElement | null>(null);
   const debugElementRef = useRef<HTMLPreElement | null>(null);
+  const svgElementRef = useRef<SVGSVGElement | null>(null);
 
   function onFrameChange(frameIndex: number) {
     if (debugElementRef.current === null) return;
     debugElementRef.current.innerHTML = "Frame: " + String(frameIndex);
+
+    const svg = svgElementRef.current;
+    d3.select(svg)
+      .select("circle")
+      .attr("cx", frameIndex)
+      .attr("cy", frameIndex)
+      .attr("r", 10)
+      .style("fill", "tomato");
   }
 
   const frameTracking = useVideoFrameTracking({
@@ -35,6 +45,10 @@ export function VideoRenderer(props: VideoRendererProps) {
         onCanPlay={frameTracking.onCanPlay}
         onSeeked={frameTracking.onSeeked}
       ></video>
+
+      <svg ref={svgElementRef} width={512} height={512}>
+        <circle r={10} fill="tomato" />
+      </svg>
 
       <pre ref={debugElementRef}></pre>
     </Box>
