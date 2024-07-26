2caecb05bee4c0281953870fea49f6d23f7ebbc2
Jirka-Mayer <jirkamayer97@gmail.com>
2024-07-26 17:00:33 -0400

    Better translation context tracker

--- backend/app/translation/SignLlavaTranslator.py
+++ backend/app/translation/SignLlavaTranslator.py
@@ -2,10 +2,35 @@ from pathlib import Path
 from ..domain.ClipsCollection import ClipsCollection
 from ..domain.Clip import Clip
 import numpy as np
+from typing import List, Optional
 
 
 from llava.sign_public_api import SignLlava, SignLlavaInput, \
     SignLlavaOutput, GenerationConfig, prepare_translation_prompt
+
+
+class ContextTracker:
+    def __init__(self, max_length: int):
+        self.max_length = max_length
+        self._previous_clips: List[str] = []
+        self._total_length = 0
+    
+    def add_next_output(self, output: str):
+        self._previous_clips.append(output)
+        self._total_length += len(output)
+    
+    def _truncate_length(self):
+        while self._total_length > self.max_length:
+            if len(self._previous_clips) == 0:
+                return
+            size_delta = len(self._previous_clips[0])
+            self._previous_clips.pop(0)
+            self._total_length -= size_delta
+
+    def get_current_context(self) -> Optional[str]:
+        if len(self._previous_clips) == 0:
+            return None
+        return " ".join(self._previous_clips)
 
 
 class SignLlavaTranslator:
@@ -45,17 +70,16 @@ class SignLlavaTranslator:
         clips_collection = ClipsCollection.load(self.clips_collection_file)
 
         # perform translation clip-by-clip
-        context = None
+        context_tracker = ContextTracker(self.MAX_CONTEXT_LENGTH)
         for clip_index, clip in enumerate(clips_collection.clips):
             frames_from = clip.start_frame
             frames_to = clip.start_frame + clip.frame_count
+            context = context_tracker.get_current_context()
             clip_data = SignLlavaInput(
                 sign2vec_features=embeddings_s2v[f"clip_{clip_index}"],
                 mae_features=embeddings_mae[frames_from:frames_to, :],
                 dino_features=embeddings_dino[frames_from:frames_to, :],
-                prompt=prepare_translation_prompt(
-                    context=context
-                ),
+                prompt=prepare_translation_prompt(context=context),
                 generation_config=GenerationConfig()
             )
             output_data: SignLlavaOutput = sign_llava.run_inference(clip_data)
@@ -63,13 +87,7 @@ class SignLlavaTranslator:
             clip.translation_context = context
             clip.translation_result = output_data.output
 
-            # update context
-            if context is None:
-                context = ""
-            context += output_data.output
-            context = context[-self.MAX_CONTEXT_LENGTH:]
-            if len(context) == self.MAX_CONTEXT_LENGTH:
-                context = "..." + context
+            context_tracker.add_next_output(output_data.output)
 
             print(
                 f"Clip {clip_index} was translated as:",
